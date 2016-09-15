2cfb17f3a487aa1f8e2aeb291bcfec6a8f83f2e5
bappa <bappa21@gmail.com>
2016-09-15 15:32:58 +0100

    Adding Version Matcher

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-release-version = $1
-echo "Need to release with release version=$release-version"
-if [  $release-version = "minor" ]; then
+releasetype=$1
+echo "Need to release with release version=$releasetype"
+if [  $releasetype = "minor" ]; then
 cd microservices/composite/product-composite-service; ./gradlew release -Prelease.useAutomaticVersion=true; cd -
 else
    echo "Could not get what type of release is wanted."
