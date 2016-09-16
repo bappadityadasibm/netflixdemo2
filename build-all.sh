cd027765311cc11d32edaf596e4036e9b531250e
bappa <bappa21@gmail.com>
2016-09-16 15:01:54 +0100

    Adding Version Matcher

--- build-all.sh
+++ build-all.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+releasetype=$1
+echo "Need to build with release version=$releasetype"
+
 cd microservices/core/product-service;                ./gradlew clean publishToMavenLocal; cd -
 cd microservices/core/recommendation-service;         ./gradlew clean publishToMavenLocal; cd -
 cd microservices/core/review-service;                 ./gradlew clean publishToMavenLocal; cd -
-cd microservices/composite/product-composite-service; ./gradlew clean build; cd -
 
+if [  $releasetype = "minor" ]; then
+cd microservices/composite/product-composite-service; ./gradlew clean build -PreleaseType=minor; cd -
+else
+cd microservices/composite/product-composite-service; ./gradlew clean build -PreleaseType=patch; cd -
+fi
 cd microservices/support/discovery-server;            ./gradlew clean build; cd -
 cd microservices/support/edge-server;                 ./gradlew clean build; cd -
 cd microservices/support/monitor-dashboard;           ./gradlew clean build; cd -
--- publish-all.sh
+++ publish-all.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-cd microservices/composite/product-composite-service; ./gradlew artifactoryPublish; cd -
+releasetype=$1
+echo "Need to publish with release version=$releasetype"
+
+if [  $releasetype = "minor" ]; then
+cd microservices/composite/product-composite-service; ./gradlew artifactoryPublish -PreleaseType=minor; cd -
+else
+cd microservices/composite/product-composite-service; ./gradlew artifactoryPublish -PreleaseType=patch; cd -
+fi
