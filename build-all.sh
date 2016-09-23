6376dc963d205bb583ae8886452f8b9307c1dc11
bappa <bappaditya.das@db.com>
2016-09-23 15:05:32 +0100

    Adding Version Matcher

--- build-all.sh
+++ build-all.sh
@@ -1,21 +1,11 @@
 #!/usr/bin/env bash
 
-releasetype=$1
-echo "Need to build with release version=$releasetype"
-
 cd microservices/core/product-service;                ./gradlew clean publishToMavenLocal; cd -
 cd microservices/core/recommendation-service;         ./gradlew clean publishToMavenLocal; cd -
 cd microservices/core/review-service;                 ./gradlew clean publishToMavenLocal; cd -
 
-if [  $releasetype = "minor" ]
-then
 cd microservices/composite/product-composite-service; ./gradlew clean build; ./gradlew asciidoctor; cd -
-elif [  $releasetype = "patch" ]
-then
-cd microservices/composite/product-composite-service; ./gradlew clean build; ./gradlew asciidoctor; cd -
-else
-cd microservices/composite/product-composite-service; ./gradlew clean build; ./gradlew asciidoctor; cd -
-fi
+
 cd microservices/support/discovery-server;            ./gradlew clean build; cd -
 cd microservices/support/edge-server;                 ./gradlew clean build; cd -
 cd microservices/support/monitor-dashboard;           ./gradlew clean build; cd -
