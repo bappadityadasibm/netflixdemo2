dd31f47aadfb52235abc58b4beacbb6cad746306
bappa <bappaditya.das@db.com>
2016-09-26 16:34:25 +0100

    Adding Version Matcher

--- build-all.sh
+++ build-all.sh
@@ -4,7 +4,15 @@ cd microservices/core/product-service;                ./gradlew clean publishToM
 cd microservices/core/recommendation-service;         ./gradlew clean publishToMavenLocal; cd -
 cd microservices/core/review-service;                 ./gradlew clean publishToMavenLocal; cd -
 
-cd microservices/composite/product-composite-service; ./gradlew clean build; cd -
+cd microservices/composite/product-composite-service; ./gradlew clean build;
+
+if [ $? -ne 0 ]; then
+
+    exit 1
+
+fi;
+
+cd -
 
 cd microservices/support/discovery-server;            ./gradlew clean build; cd -
 cd microservices/support/edge-server;                 ./gradlew clean build; cd -
